875db2a99624d043b32b528b685d064f50f129f5
Ula Jeziorna <jeziornaula@wp.pl>
2023-10-08 17:22:04 +0200

    Adding new post

--- src/views/Home.tsx
+++ src/views/Home.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React, { ChangeEvent, FormEvent, useEffect, useState } from "react";
 
 import axios, { AxiosResponse } from "axios";
 import { ObjectContext, Post } from "../types";
@@ -14,6 +14,12 @@ export default function Home() {
   const obj: ObjectContext = useOutletContext();
 
   const [posts, setPosts] = useState<Post[]>([]);
+  const [newPostContent, setNewPostContent] = useState<string>('');
+
+
+
+  axios.defaults.headers.common['Authorization'] = "Bearer " + (obj.loggedUser.jwt_token.length > 0 ? obj.loggedUser.jwt_token : '');
+
 
   const getLatestPosts = () => {
     axios.post(`${API_URL}/post/latest`).then(
@@ -28,13 +34,26 @@ export default function Home() {
       })
   }
 
-  const getNextPosts = () => {
+  const getNewestPosts = () => {
+    axios.post(`${API_URL}/post/newer-then`, {
+      date: posts[0].created_at
+    }).then(
+      (response: AxiosResponse<any>) => {
+        // how to map to correct interface type?
+        setPosts((response.data as Post[]).concat(posts));
+      }
+    )
+      .catch((error) => {
+        console.error('An error has occured:', error);
+      })
+  }
+
+  const getOlderPosts = () => {
     axios.post(`${API_URL}/post/older-then`, {
       date: posts[posts.length - 1].created_at
     }).then(
       (response: AxiosResponse<any>) => {
         // how to map to correct interface type?
-
         setPosts(posts.concat(response.data as Post[]));
       }
     )
@@ -42,6 +61,28 @@ export default function Home() {
         console.error('An error has occured:', error);
       })
   }
+
+
+  const addNewPost = (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    axios.post(`${API_URL}/post/add`, {
+      content: newPostContent
+    }).then(
+      (response: AxiosResponse<any>) => {
+        if (response.status === 200) {
+          setNewPostContent('');
+          getNewestPosts();
+        }
+      }
+    )
+      .catch((error) => {
+        console.error('An error has occurred during adding new post:', error);
+      })
+  }
+
+
+
+
 
   useEffect(() => {
     getLatestPosts();
@@ -54,8 +95,14 @@ export default function Home() {
     <div className="HomeContainer">
       {obj.loggedUser.jwt_token.length > 0 &&
         <div className="NewPostContainer">
-          <textarea rows={3} className="NewPostTextarea" placeholder="Write new post" />
-          <button className="NewPostAddButton">Add</button>
+          <form className="NewPostForm" onSubmit={(event: FormEvent<HTMLFormElement>) => addNewPost(event)}>
+            <textarea rows={3}
+              onChange={(e: ChangeEvent<HTMLTextAreaElement>) => setNewPostContent(e.target.value)}
+              value={newPostContent}
+              className="NewPostTextarea"
+              placeholder="Write new post" />
+            <button className="NewPostAddButton">Add</button>
+          </form>
         </div>
       }
       <div className="PostList">
@@ -66,7 +113,7 @@ export default function Home() {
           }
         )}
       </div>
-      <button className="LoadMoreButton" onClick={getNextPosts}>Load more</button>
+      <button className="LoadMoreButton" onClick={getOlderPosts}>Load more</button>
     </div>
   )
 }
